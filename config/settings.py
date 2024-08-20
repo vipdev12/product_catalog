8f1aa122e7b8685564b2cc0cf792cdc9eb95d908
Dinislam <aikenamuteshova@gmail.com>
2024-08-20 23:49:18 +0600

    v1

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': os.environ.get('POSTGRES_NAME'),
+       'USER': os.environ.get('POSTGRES_USER'),
+       'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+       'HOST': os.environ.get('POSTGRES_HOST'),
+       'PORT': os.environ.get('POSTGRES_PORT'),
+   }
 }
 
 
